ae7f8f2fcc0462f74fc42309579f38967016c9d2
Dawid Klos <klosdawid12@gmail.com>
2020-12-18 14:08:23 +0000

    Add styles for repositories.

--- src/js/index.js
+++ src/js/index.js
@@ -19,10 +19,10 @@ fetch("https://api.github.com/users/Dawid-Klos/repos?sort=created&direction=asc"
     const repositoryList = document.querySelector('.project--js');
     const myTemplate = `
     <ul class="project">
-    <li> project: <strong>${name}</strong> </li>
-    <li>description: ${description} <br /> </li>
-    <li>demo: <a href="${html_url}" title="This is link to ${name}    repository from my GitHub list">see here</a></li>
-    <li>github: <a href="https://github.com/Dawid-Klos/${name}" alt="This is a soruce code of ${name} project.">source-code</a></li>
+    <li class="project__name"><span class="project__name--left">project</span> : <span class="project__content--right"><strong>${name}</strong></span> </li>
+    <li class="project__description"><span class="project__name--left">description</span> : <span class="project__content--right">${description}</span> </li>
+    <li class="project__demo"><span class="project__name--left">demo</span> : <span class="project__content--right"><a href="${html_url}" title="This is link to ${name} repository from my GitHub list">see here</a></span></li>
+    <li class="project__source"><span class="project__name--left">github</span> : <span class="project__content--right"><a href="https://github.com/Dawid-Klos/${name}" alt="This is a soruce code of ${name} project.">source-code</a></span></li>
     </ul>
     `;
      repositoryList.innerHTML += myTemplate;
