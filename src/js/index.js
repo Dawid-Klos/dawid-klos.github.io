16e85f50fe7de6b602608f0e468791e7cf8d1750
Dawid Klos <klosdawid12@gmail.com>
2020-12-19 15:14:37 +0000

    Finish section with projects.

--- src/js/index.js
+++ src/js/index.js
@@ -19,10 +19,21 @@ fetch("https://api.github.com/users/Dawid-Klos/repos?sort=created&direction=asc"
     const repositoryList = document.querySelector('.project--js');
     const myTemplate = `
     <ul class="project">
-    <li class="project__name"><span class="project__name--left">project</span> : <span class="project__content--right"><strong>${name}</strong></span> </li>
-    <li class="project__description"><span class="project__name--left">description</span> : <span class="project__content--right">${description}</span> </li>
-    <li class="project__demo"><span class="project__name--left">demo</span> : <span class="project__content--right"><a href="${html_url}" title="This is link to ${name} repository from my GitHub list">see here</a></span></li>
-    <li class="project__source"><span class="project__name--left">github</span> : <span class="project__content--right"><a href="https://github.com/Dawid-Klos/${name}" alt="This is a soruce code of ${name} project.">source-code</a></span></li>
+    <li class="name">
+    <span class="name__left project--left">project</span>:<span class="name__content project--right"><strong>${name}</strong></span>
+     </li>
+
+    <li class="description">
+    <span class="description__left project--left">description</span>:<span class="description__content project--right">${description}</span> 
+    </li>
+
+    <li class="demo">
+    <span class="demo__left project--left">demo</span>:<span class="demo__content project--right">\<<a class="project--link" href="${html_url}" title="This is link to ${name} repository from my GitHub list">see here</a>\></span>
+    </li>
+
+    <li class="source">
+    <span class="source__left project--left">github</span>:<span class="source__content project--right">\<<a class="project--link" href="https://github.com/Dawid-Klos/${name}" alt="This is a soruce code of ${name} project.">source-code</a>\></span>
+    </li>
     </ul>
     `;
      repositoryList.innerHTML += myTemplate;
