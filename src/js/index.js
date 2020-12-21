2a7e5b6d0626d13530fd429d676cb97ba2141d7b
Dawid Klos <klosdawid12@gmail.com>
2020-12-21 20:08:54 +0000

    Add media queries for mobile

--- src/js/index.js
+++ src/js/index.js
@@ -10,12 +10,12 @@ console.log('HELLO 🚀')
 
 
 const projectsList = document.querySelector('.project--js');
-const counter = 0;
+
 fetch("https://api.github.com/users/Dawid-Klos/repos?sort=created&direction=desc")
   .then(resp => resp.json())
   .then(resp => {
    for (let repo of resp) {
-    const {name, description, html_url} = repo;
+    const {name, description, html_url, homepage} = repo;
     const repositoryList = document.querySelector('.project--js');
     const myTemplate = `
     <ul class="project">
@@ -28,20 +28,17 @@ fetch("https://api.github.com/users/Dawid-Klos/repos?sort=created&direction=desc
     </li>
 
     <li class="demo">
-    <span class="demo__left project--left">demo</span>:<span class="demo__content project--right">\<<a class="project--link" href="${html_url}" title="This is link to ${name} repository from my GitHub list">see here</a>\></span>
+    <span class="demo__left project--left">demo</span>:<span class="demo__content project--right">\<<a class="project--link" href="${homepage}" target="_blank" alt="This is link to ${name} repository from my GitHub list">see here</a>\></span>
     </li>
 
     <li class="source">
-    <span class="source__left project--left">github</span>:<span class="source__content project--right">\<<a class="project--link" href="https://github.com/Dawid-Klos/${name}" alt="This is a soruce code of ${name} project.">source-code</a>\></span>
+    <span class="source__left project--left">github</span>:<span class="source__content project--right">\<<a class="project--link" href="${html_url}" target="_blank" alt="This is a soruce code of ${name} project.">source-code</a>\></span>
     </li>
     </ul>
     `;
     
-    if(counter < 7) {
       repositoryList.innerHTML += myTemplate;
-      counter++;
-    }
-    
+
   }
 })
   .catch(error => {
