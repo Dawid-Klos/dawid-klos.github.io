07600b7178062eba10872f94f49c76911386bd07
Dawid Klos <klosdawid12@gmail.com>
2021-01-15 11:48:44 +0000

    Smoth scrolling

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
 import '../scss/main.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -8,8 +11,15 @@ import '../scss/main.scss';
 
 console.log('HELLO 🚀')
 
+// Animations
 
+AOS.init();
+
+
+
+// Downloading projects from Github
 const projectsList = document.querySelector('.project--js');
+const counter = 1;
 
 fetch("https://api.github.com/users/Dawid-Klos/repos?sort=created&direction=desc")
   .then(resp => resp.json())
@@ -17,8 +27,8 @@ fetch("https://api.github.com/users/Dawid-Klos/repos?sort=created&direction=desc
    for (let repo of resp) {
     const {name, description, html_url, homepage} = repo;
     const repositoryList = document.querySelector('.project--js');
-    const myTemplate = `
-    <ul class="project">
+    const myTemplateLeft = `
+    <ul class="project" data-aos="slide-left">
     <li class="name">
     <span class="name__left project--left">project</span>:<span class="name__content project--right"><strong>${name}</strong></span>
      </li>
@@ -36,14 +46,39 @@ fetch("https://api.github.com/users/Dawid-Klos/repos?sort=created&direction=desc
     </li>
     </ul>
     `;
-    
-      repositoryList.innerHTML += myTemplate;
+    const myTemplateRight = `
+    <ul class="project" data-aos="slide-right">
+    <li class="name">
+    <span class="name__left project--left">project</span>:<span class="name__content project--right"><strong>${name}</strong></span>
+     </li>
 
+    <li class="description">
+    <span class="description__left project--left">description</span>:<span class="description__content project--right">${description}</span> 
+    </li>
+
+    <li class="demo">
+    <span class="demo__left project--left">demo</span>:<span class="demo__content project--right">\<<a class="project--link" href="${homepage}" target="_blank" rel="noopener" alt="This is link to ${name} repository from my GitHub list">see here</a>\></span>
+    </li>
+
+    <li class="source">
+    <span class="source__left project--left">github</span>:<span class="source__content project--right">\<<a class="project--link" href="${html_url}" target="_blank" rel="noopener" alt="This is a soruce code of ${name} project.">source-code</a>\></span>
+    </li>
+    </ul>
+    `;
+    if ((counter % 2) == 0) {
+      repositoryList.innerHTML += myTemplateLeft;
+    } else {
+      repositoryList.innerHTML += myTemplateRight;
+    }
+    counter++;
   }
 })
   .catch(error => {
   console.log('error');
 })
+
+
+// Animated action on mail button
 
 const mail = document.querySelector('.mail--js');
 const mailButton = document.querySelector('.mail-button--js');
